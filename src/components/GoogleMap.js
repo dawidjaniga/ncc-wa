d93a10b0439b77e4f97302a9e30eb2cfbec3fd80
Dawid Janiga <dawid.janiga@netguru.com>
2020-11-25 01:28:45 +0100

    feat: load Google Places

--- src/components/GoogleMap.js
+++ src/components/GoogleMap.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from 'react'
+import React, { useEffect } from 'react'
 import styled from 'styled-components'
 import { Loader } from '@googlemaps/js-api-loader'
 
@@ -10,7 +10,8 @@ const StyledMap = styled.div.attrs({
 
 const loader = new Loader({
   apiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY,
-  version: 'weekly'
+  version: 'weekly',
+  libraries: ['places']
 })
 
 let map
@@ -25,13 +26,63 @@ export default function GoogleMap ({ onCenterChanged, markers }) {
           // minZoom: 14
         })
 
+        const input = document.getElementById('search-address')
+        const searchBox = new window.google.maps.places.SearchBox(input)
+
         map.addListener('dragend', (...args) => {
           onCenterChanged(map.getCenter())
+        })
+
+        searchBox.addListener('places_changed', () => {
+          const places = searchBox.getPlaces()
+          console.log(places)
+
+          if (places.length == 0) {
+            return
+          }
+          // Clear out the old markers.
+          markers.forEach(marker => {
+            marker.setMap(null)
+          })
+          markers = []
+          // For each place, get the icon, name and location.
+          const bounds = new window.google.maps.LatLngBounds()
+          places.forEach(place => {
+            if (!place.geometry) {
+              console.log('Returned place contains no geometry')
+              return
+            }
+            const icon = {
+              url: place.icon,
+              size: new window.google.maps.Size(71, 71),
+              origin: new window.google.maps.Point(0, 0),
+              anchor: new window.google.maps.Point(17, 34),
+              scaledSize: new window.google.maps.Size(25, 25)
+            }
+            // Create a marker for each place.
+            markers.push(
+              new window.google.maps.Marker({
+                map,
+                icon,
+                title: place.name,
+                position: place.geometry.location
+              })
+            )
+
+            if (place.geometry.viewport) {
+              // Only geocodes have viewport.
+              bounds.union(place.geometry.viewport)
+            } else {
+              bounds.extend(place.geometry.location)
+            }
+          })
+          map.fitBounds(bounds)
         })
       } else {
         console.error('Google API not loaded')
       }
     })
+    // eslint-disable-next-line
   }, [])
 
   useEffect(() => {
