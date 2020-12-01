3e98e7719da94a11dbe3ea2ad348c014691a0ada
Dawid Janiga <dawid.janiga@netguru.com>
2020-12-01 20:58:33 +0100

    refactor: extract Marker component

--- src/components/GoogleMap.js
+++ src/components/GoogleMap.js
@@ -1,53 +1,11 @@
-import React, { useState } from 'react'
-import styled from 'styled-components'
+import React from 'react'
+
 import useMapPageMediator from 'pages/map/mediator'
 import GoogleMapReact from 'google-map-react'
-import { Tooltip } from 'antd'
-import { useCurrentMarkerStore, useMapStore, useMarkerStore } from 'pages/map/store'
+import { useCurrentMarkerStore, useMapStore } from 'pages/map/store'
 import styles from './MapStyles'
 import { Modal } from 'antd'
-
-const colors = {
-  orange: {
-    background: '#ff7e23e0',
-    shadow: '#ffa769'
-  },
-  red: {
-    background: '#ff2323b5',
-    shadow: '#ff6969'
-  }
-}
-
-const Circle = styled.div`
-  background-color: ${props => colors[props.color].background};
-  border-radius: 50%;
-  width: 30px;
-  height: 30px;
-  box-shadow: 0px 0px 5px ${props => colors[props.color].shadow};
-  opacity: 0.7;
-  transition: all 0.2s ease-in;
-  cursor: pointer;
-  transform: scale(1);
-
-  &:hover {
-    opacity: 1;
-    /* transform: scale(1.2); */
-  }
-`
-
-function Marker ({ title, url, color, onClick }) {
-  const { userClickedMarker } = useMapPageMediator()
-
-  function handleOnClick () {
-    userClickedMarker(title)
-  }
-
-  return (
-    <Tooltip title={title} onClick={handleOnClick}>
-      <Circle color={color} />
-    </Tooltip>
-  )
-}
+import Marker from './Marker'
 
 function ArticleModal() {
   const [{ visible }] = useMapStore()
---
+++ src/components/Marker.js
@@ -0,0 +1,44 @@
+import styled from 'styled-components'
+import { Tooltip } from 'antd'
+import useMapPageMediator from 'pages/map/mediator'
+
+const colors = {
+    orange: {
+      background: '#ff7e23e0',
+      shadow: '#ffa769'
+    },
+    red: {
+      background: '#ff2323b5',
+      shadow: '#ff6969'
+    }
+  }
+  
+  const Circle = styled.div`
+    background-color: ${props => colors[props.color].background};
+    border-radius: 50%;
+    width: 30px;
+    height: 30px;
+    box-shadow: 0px 0px 5px ${props => colors[props.color].shadow};
+    opacity: 0.7;
+    transition: all 0.2s ease-in;
+    cursor: pointer;
+    transform: scale(1);
+  
+    &:hover {
+      opacity: 1;
+    }
+  `
+  
+  export default function Marker ({ title, url, color, onClick }) {
+    const { userClickedMarker } = useMapPageMediator()
+  
+    function handleOnClick () {
+      userClickedMarker(title)
+    }
+  
+    return (
+      <Tooltip title={title} onClick={handleOnClick}>
+        <Circle color={color} />
+      </Tooltip>
+    )
+  }
